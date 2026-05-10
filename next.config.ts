fae8d27d67365999c30383ca62b8a9726f14f457
Alok Kumar <alokraj016@outlook.com>
2026-05-11 00:49:57 +0530

    chore: update next config base path for development and add node_modules to gitignore

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/v2',
+  basePath: isProd ? '/v2' : '',
 };
 
 export default nextConfig;
